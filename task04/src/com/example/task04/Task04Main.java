166da0351c50d86438de7d8e3647bacce21935d0
AetbaevaJulia <aetbaevajulia05@gmail.com>
2024-12-12 00:55:47 +0500

    Аетбаева Юлия. Задание 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,32 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
 
-        // your implementation here
+        buffer.lines()
+                .flatMap(x -> Stream.of(x.split("\\P{L}")) // разделяем по всем символам, которые не буквы
+                .filter(t -> !t.isEmpty())
+                .map(String::toLowerCase))
 
+                .collect(Collectors.groupingBy(t -> t, Collectors.counting()))
+                .entrySet()
+                .stream()
+
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .forEach(t -> System.out.print(t + '\n'));
     }
 
 }
